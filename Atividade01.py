d09f836b1ed96af18699546440712a1140b61730
Isaac Vilton Ribeiro <isaac.ribeiro.018@ufrn.edu.br>
2026-03-06 15:12:07 -0300

    Atividade01 - segundo commit

--- Atividade01.py
+++ Atividade01.py
@@ -1,5 +1,6 @@
 Nome = input("Digite o nome: ")
 Login = input("Digite o email: ")
 Senha = input("Digite a senha: ")
+print("Cadastro realizado com sucesso!")
 
-print(f"O perfil do usuário é: {Nome}; {Login}; {Senha}")
+print(f"O perfil do usuário é: \n{Nome} \n{Login} \n{Senha}")
